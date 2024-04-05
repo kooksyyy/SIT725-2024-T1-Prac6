a3850d229f486a6ddb7a963e0d29a99ace3d22f9
kooksyyy <kookaiisidro01@gmail.com>
2024-04-05 11:47:55 +1100

    prac 4

--- server.js
+++ server.js
@@ -1,12 +1,85 @@
-var express = require("express");
-var app = express();
+let express = require('express');
+let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://kooksyyy:Pandapenguin13!@cluster13.aj1wlnp.mongodb.net/?retryWrites=true&w=majority";
+let port = process.env.port || 3000;
+let collection;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-var port = process.env.PORT || 3000;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
-app.listen(port, ()=>{
-  console.log("App listening to: "+port);
+app.get('/', function (req, res) {
+    res.render('public/index.html');
+});
+
+app.get('/api/cats', (req, res) => {
+    if (collection) {
+        getAllCats((err, result) => {
+            if (!err) {
+                res.json({ statusCode: 200, data: result, message: 'get all cats successful' });
+            }
+        });
+    } else {
+        res.status(500).json({ statusCode: 500, message: 'Internal Server Error: Database not initialized' });
+    }
+});
+
+app.post('/api/cat', (req, res) => {
+    if (collection) {
+        let cat = req.body;
+        postCat(cat, (err, result) => {
+            if (!err) {
+                res.json({ statusCode: 201, data: result, message: 'success' });
+            }
+        });
+    } else {
+        res.status(500).json({ statusCode: 500, message: 'Internal Server Error: Database not initialized' });
+    }
+});
+
+
+function postCat(cat, callback) {
+  collection.insertOne(cat, (err, result) => {
+    if (err) {
+      console.error(err);
+      callback(err, null); // Pass error to callback
+    } else {
+      callback(null, result); // Pass result to callback
+    }
+  });
+}
+
+function getAllCats(callback) {
+  collection.find({}).toArray((err, result) => {
+    if (err) {
+      console.error(err);
+      callback(err, null); // Pass error to callback
+    } else {
+      callback(null, result); // Pass result to callback
+    }
+  });
+}
+
+async function runDBConnection() {
+  try {
+      await client.connect();
+      collection = client.db().collection('Cat');
+      console.log(collection);
+  } catch (ex) {
+      console.error(ex);
+  }
+}
+
+app.listen(port, () => {
+    console.log('express server started');
+    runDBConnection();
 });
