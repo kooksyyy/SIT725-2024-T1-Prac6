cb5e9a22898da231a2ba8f3d2b6972628b2f8f14
kooksyyy <kookaiisidro01@gmail.com>
2024-04-05 12:10:13 +1100

    modified script

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,17 +19,18 @@ const clickMe = () => {
 }
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
-                '</div><div class="card-content">'+
-                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#"></a></p></div>'+
-                '<div class="card-reveal">'+
-                '<span class="card-title grey-text text-darken-4">'+item.subtitle+'<i class="material-icons right">close</i></span>'+
-                '<p class="card-text">'+item.description+'</p>'+
-                '</div></div></div>';
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.description + '</p>' +
+            '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
+
 
 const submitForm = () => {
     let formData = {};
@@ -69,7 +70,7 @@ $(document).ready(function(){
     $('#formSubmit').click(()=>{
         submitForm();
     });
-    $('.modal').modal();
     addCards(cardList);
+    $('.modal').modal();
     getAllCats();
 });
